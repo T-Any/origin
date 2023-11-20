1c14c193224a7c019d16fcb20b5cc414c27f377d
Your Name <you@example.com>
2023-11-20 22:41:15 +0200

    add example007_Magic

--- example007_Magic/Program.cs
+++ example007_Magic/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Clear();
+﻿//Console.Clear();
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("+");
 
@@ -20,7 +20,7 @@ Console.WriteLine("+");
 
 int x = xa, y = xb;
 int count = 0;
-while(count < 10);
+while(count < 10000);
 {
     int what = new Random().Next(0, 3 ); //[0;3] 0 1 2 
     if( what == 0 )
@@ -28,19 +28,19 @@ while(count < 10);
         x = (x + xa) / 2;
         y = (y + ya) / 2;
     }
-if (what == 1)
-{
-    x = (x + xb) / 2;
-    y = (y + yb) / 2;
-}
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
 
-if (what == 2)
-{
-    x = (x + xc) / 2;
-    y = (y + yc) / 2;
-}
+    if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
 
-Console.SetCursorPosition(x, y);
-Console.WriteLine("+");
-count = count + 1;
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count = count + 1;
 }
